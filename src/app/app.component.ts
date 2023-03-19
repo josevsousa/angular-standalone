27a3986a4f87deb53965073a4874ffac1527d3ec
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-03-19 16:54:03 -0300

    convetido para standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+// import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
